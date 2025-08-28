9f5c8021a6036f048c7805ff5957639e6cfc3087
joaovictor04 <79606553+joaovictor04@users.noreply.github.com>
2025-08-28 20:19:15 -0300

    Melhoria de contador.

--- js/jogo.js
+++ js/jogo.js
@@ -1,15 +1,18 @@
 let rangeMinAdvinhar = 1;
-let rangeMaxAdivinhar = 15;
+let rangeMaxAdivinhar = 30;
+let tentativas = 10;
+let txtTentativa = tentativas > 1 ? "tentativas" : "tentativa";
 let numSorteado = geraNumero();
-
+let msg = alert("O jogo funciona da sequinte forma: \nVocê deve informar o valor no campo abaixo e clicar em chutar.\n10 é o maximo de tentativas, caso ultrapasse o jogo é iniciado novamente. Um numero novo é gerado. \nCaso queira jogar novamente, basta clicar em Novo game.\n\n>>>> Boa Sorte <<<<");
 
 function exibirNaTela (tag, msg) {
     let campo = document.querySelector(tag);
     campo.innerHTML = msg;
 }
 
-exibirNaTela("#texto_Input",`Escolha um numero para jogar de ${rangeMinAdvinhar} a ${rangeMaxAdivinhar}`)
-exibirNaTela("#resultado_valor", "Resultado...")
+exibirNaTela("#texto_Input",`Adivinhe o numero entre ${rangeMinAdvinhar} e ${rangeMaxAdivinhar}`);
+exibirNaTela("#resultado_valor", "");
+msg;
 
 function geraNumero(){
     let numero = (Math.random() * (rangeMaxAdivinhar - rangeMinAdvinhar) + rangeMinAdvinhar);
@@ -18,22 +21,40 @@ function geraNumero(){
 
 function chute (){
     let numChute = document.getElementById("chutar").value;
-        
+
     if (numChute == numSorteado) {
-        exibirNaTela("#resultado_valor", `Acertou, o numero é ${numSorteado}`);
+        exibirNaTela("#resultado_valor", "Acertou");
     }
     else{
-        exibirNaTela("#resultado_valor", "Errou");
+        tentativas-=1;
+        if(tentativas==0){
+            exibirNaTela("#resultado_valor", `Você perdeu! O número secreto era ${numSorteado}\nIniciando novo jogo...`);
+            exibirNaTela("#qtdTentativa","");
+            setTimeout(novoJogo,5000);
+        }
+        else{
+            if (numChute > numSorteado){
+                exibirNaTela("#resultado_valor", "Numero acima");
+            }else{
+                exibirNaTela("#resultado_valor", "Numero abaixo");
+            }
+        exibirNaTela("#qtdTentativa", `${tentativas} ${txtTentativa}`);
+        }
     }
-    limpar();
+    document.getElementById("chutar").value = "";
+    return tentativas;
 }
+
 
 function novoJogo(){
     numSorteado = geraNumero();
-    console.log(numSorteado);
     limpar();
 }
 
 function limpar() {
     document.getElementById("chutar").value = "";
+    exibirNaTela("#resultado_valor", "");
+    exibirNaTela("#qtdTentativa", "");
+    tentativas=10;
 }
+
